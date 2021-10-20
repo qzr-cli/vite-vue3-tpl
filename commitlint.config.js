931f393d915b6606b0caafe64844cad2c86a717a
邱卓然 <z5021996@vip.qq.com>
2021-10-20 16:14:29 +0800

    feat(development): 添加commit约束

---
+++ commitlint.config.js
@@ -0,0 +1,9 @@
+/**
+ * @Date         : 2021-10-20 15:14:25
+ * @Description  :
+ * @Autor        : Qzr(z5021996@vip.qq.com)
+ * @LastEditors  : Qzr(z5021996@vip.qq.com)
+ * @LastEditTime : 2021-10-20 15:14:27
+ */
+
+module.exports = { extends: ['@commitlint/config-conventional'] }
